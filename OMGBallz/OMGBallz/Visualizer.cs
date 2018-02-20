c501062341a7b9b4acc8452716b873f4d4bd2714
Niek Mulleners <niekmulleners@gmail.com>
2018-02-20 16:46:36 +0100

    It's Alive!

--- OMGBallz/OMGBallz/Visualizer.cs
+++ OMGBallz/OMGBallz/Visualizer.cs
@@ -30,42 +30,43 @@ public class Visualizer : Form
 
         PhysicsObject[] ballz = new PhysicsObject[]
             { new Ball(10, new Vector(100, 100))
-            //, new Ball(10, new Vector(100, 60), new Vector(0, 1))
-            //, new Ball(10, new Vector(100, 160), new Vector(0, -2))
+            , new Ball(10, new Vector(105, 40), new Vector(0, 1))
+            , new Ball(10, new Vector(100, 160), new Vector(0, -2))
             , new Ball(10, new Vector(220, 120), new Vector(-1, 0))
             //, new Ball(10, new Vector(100, 130))
-            //, new Ball(10, new Vector(10, 130), new Vector(3, 0))
+            , new Ball(10, new Vector(10, 130), new Vector(3, 0))
             //, new Ball(10, new Vector(100, 160))
-            //, new Ball(10, new Vector(10, 160), new Vector(3, 0))
+            , new Ball(10, new Vector(10, 160), new Vector(3, 0))
             };
 
         world = new World(ballz);
 
-        KeyPreview = true;
+        //KeyPreview = true;
 
-        KeyDown += (_, e) =>
-        {
-            if (e.KeyCode == Keys.Space)
-            {
-                world.Update();
-                Render(world.Objects);
-            }
-        };
+        //KeyDown += (_, e) =>
+        //{
+        //    if (e.KeyCode == Keys.Space)
+        //    {
+        //        world.Update(10);
+        //        Render(world.Objects);
+        //    }
+        //};
 
         Render(world.Objects);
 
-        //Timer timer = new Timer
-        //    { Interval = 1
-        //    };
+        Timer timer = new Timer
+        {
+            Interval = 1
+        };
 
-        //timer.Tick += (sender, args) =>
-        //{
-        //    world.Update(2f);
-        //    Render(world.Objects);
-        //    pictureBox.Update();
-        //};
+        timer.Tick += (sender, args) =>
+        {
+            world.Update(0.2f);
+            Render(world.Objects);
+            //pictureBox.Update();
+        };
 
-        //timer.Start();
+        timer.Start();
     }
 
     public void Render(IEnumerable<PhysicsObject> objects)
--- OMGBallz/OMGBallz/World.cs
+++ OMGBallz/OMGBallz/World.cs
@@ -16,10 +16,7 @@ public class World
     {
         Objects = objects;
         collisions = new List<(double, PhysicsObject, PhysicsObject)>();
-    }
-
-    public void Update()
-    {
+        
         for (int i = 0; i < Objects.Count(); i++)
             for (int j = i + 1; j < Objects.Count(); j++)
             {
@@ -27,19 +24,79 @@ public class World
 
                 collisions.Add((first.Collides(second), first, second));
             }
+    }
+
+    public void Update(double dt)
+    {
+        //for (int i = 0; i < Objects.Count(); i++)
+        //    for (int j = i + 1; j < Objects.Count(); j++)
+        //    {
+        //        PhysicsObject first = Objects[i], second = Objects[j];
+
+        //        collisions.Add((first.Collides(second), first, second));
+        //    }
 
         collisions.Sort((alpha, beta) => alpha.Item1.CompareTo(beta.Item1));
 
-        (double dt, PhysicsObject a, PhysicsObject b) = collisions.First();
+        (double next, PhysicsObject a, PhysicsObject b) = collisions.First();
 
-        foreach (PhysicsObject obj in Objects)
-            obj.Update(dt);
+        double timeStep = dt;
+
+        if (next < dt)
+        {
+            foreach (PhysicsObject obj in Objects)
+                obj.Update(next);
+
+            a.HandleCollision(b);
+            b.HandleCollision(a);
+            a.ApplyCollision();
+            b.ApplyCollision();
+
+            timeStep = next;
+
+            var newCollisions = new List<(double, PhysicsObject, PhysicsObject)>();
+
+            foreach (var collision in collisions)
+            {
+                PhysicsObject first = collision.Item2, second = collision.Item3;
+
+                if (first == a || first == b || second == a || second == b)
+                    newCollisions.Add((first.Collides(second), first, second));
+                else
+                    newCollisions.Add((collision.Item1 - timeStep, first, second));
+            }
+
+            collisions = newCollisions;
+
+            Update(dt - timeStep);
+        }
+        else
+        {
+            foreach (PhysicsObject obj in Objects)
+                obj.Update(timeStep);
+
+            var newCollisions = new List<(double, PhysicsObject, PhysicsObject)>();
+
+            foreach (var collision in collisions)
+            {
+                PhysicsObject first = collision.Item2, second = collision.Item3;
+                
+                newCollisions.Add((collision.Item1 - timeStep, first, second));
+            }
+
+            collisions = newCollisions;
+        }
+
         
-        a.HandleCollision(b);
-        b.HandleCollision(a);
-        a.ApplyCollision();
-        b.ApplyCollision();
+        
+        //collisions = collisions.Select<(double, PhysicsObject, PhysicsObject),(double, PhysicsObject, PhysicsObject)>((time, first, second) =>
+        //{
+        //    if (first == a || first == b || second == a || second == b)
+        //        return (first.Collides(second), first, second);
+        //    else
+        //        return (time - timeStep, first, second);
+        //}).ToList();
 
-        collisions.Clear();
+        
     }
 }
